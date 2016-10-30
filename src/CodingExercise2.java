3c6262ad2715111c578f1b9a10953cc7ef373186
703233 <league@WTS5.attlocal.net>
2016-10-30 12:58:34 -0700

    calendar
    calendar

--- src/CodingExercise2.java
+++ src/CodingExercise2.java
@@ -1,10 +1,14 @@
+import java.util.Calendar;
+
 import javax.swing.JOptionPane;
 
 public class CodingExercise2 {
 public static void main(String[] args) {
+	Calendar now = Calendar.getInstance();
 	String x = JOptionPane.showInputDialog("How old are you?");
-	int age = Integer.parseInt(x);
-	int year = 2016 - age;
+	int age = Integer.parseInt(x); 
+	now.add(Calendar.YEAR, -age);
+	int year = now.get(Calendar.YEAR);
 	if(age >= 30){
 		JOptionPane.showMessageDialog(null, "You are too old to play this game.");
 	}
